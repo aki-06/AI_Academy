3fedd3c8b26c7ef7fa75d8460a8049b83e008dea
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-01-11 22:02:17 +0900

    変数

---
+++ 20180111.py
@@ -0,0 +1,16 @@
+# 変数
+# データを保存する箱
+lang = 'Python' # langという変数に文字列'Python'を代入
+x = 5 # xという変数に5という数値を代入
+
+# 大文字と小文字は区別される
+var = 'var'
+Var = 'Var'
+
+print(var)
+print(Var)
+
+# 二単語繋げる場合は「_」で繋げる
+hello_world = 'Hello World'
+print(hello_world)
+
