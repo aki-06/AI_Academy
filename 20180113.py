7cec6a79abb6343d910412c2a07425f69ec45a37
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-01-13 21:57:15 +0900

    Bool型

---
+++ 20180113.py
@@ -0,0 +1,26 @@
+# Pythonの型
+## Bool型
+
+"""
+True、Falseをもつ型
+"""
+"""
+A and B
+AもBもTrueならTrue
+"""
+
+"""
+A or B
+AかBがTrueならTrue
+"""
+
+"""
+A == B
+AとBが同じならTrue
+"""
+
+"""
+A != B
+AとBが違うとTrue
+"""
+
