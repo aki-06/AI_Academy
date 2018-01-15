09a6df8bf2d543529f0217bcce1f2f41f3de934e
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-01-15 23:59:18 +0900

    リストの更新

--- 20180115.py
+++ 20180115.py
@@ -23,3 +23,19 @@ print(li[-7:-6]) # []
 print(li[1:]) # [5, 10.0, 'a', 'b']
 print(li[:-1]) # [3, 5, 10.0, 'a']
 print(li[:]) # [3, 5, 10.0, 'a', 'b']
+
+
+# リストの更新
+li = []
+li = ['Python']
+print(li) # ['Python']
+li.append('PHP')
+print(li) # ['Python', 'PHP']
+li.remove('Python')
+print(li) # ['PHP']
+del li[0]
+print(li) # []
+
+li = [1, 2, 3, 4 ,5]
+li[1:4] = ['Python']
+print(li) # [1, 'Python', 5]
