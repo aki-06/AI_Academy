1eb7b1257898a0aab1ce570764e1dde71d68dbd9
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-01-18 23:16:29 +0900

    演習問題3

--- 20180118.py
+++ 20180118.py
@@ -168,3 +168,14 @@ for i in range(1, 101):
 		l.append(i)
 
 print(l)
+
+# 演習問題3
+for i in range(1, 101):
+	if i % 3 == 0 and i % 5 == 0:
+		print('FizzBuzz')
+	elif i % 3 == 0:
+		print('Fizz')
+	elif i % 5 == 0:
+		print('Buzz')
+	else:
+		print(i)
